ed9e818fa7b9c15136fa137d08c9273fe0bdf077
drakezair <andradex.js07@gmail.com>
2018-11-13 00:37:58 -0500

    Updates

---
+++ precache-manifest.a1d0a05ec616f18a029e2aca6eaab344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fb7bf857b57c5e22c39f37b7dbde079",
+    "url": "/carsend/static/media/car.6fb7bf85.png"
+  },
+  {
+    "revision": "957427b8425689d1f1a4610dd3469422",
+    "url": "/carsend/static/media/Login.957427b8.jpg"
+  },
+  {
+    "revision": "596243dc597f5bc10d0e",
+    "url": "/carsend/static/js/runtime~main.596243dc.js"
+  },
+  {
+    "revision": "250bb3491e4eee84cea7",
+    "url": "/carsend/static/js/main.250bb349.chunk.js"
+  },
+  {
+    "revision": "9dcbcc5f395a380bfd30",
+    "url": "/carsend/static/js/1.9dcbcc5f.chunk.js"
+  },
+  {
+    "revision": "250bb3491e4eee84cea7",
+    "url": "/carsend/static/css/main.b199717f.chunk.css"
+  },
+  {
+    "revision": "afd04c786a47ae1aa6f7de2f6d61d15a",
+    "url": "/carsend/index.html"
+  }
+];
